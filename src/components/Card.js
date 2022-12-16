bd4dc69a6d6e63fda2e202e8ad2a483d1f4cba19
Marcello Mulas <marcellomulas93@gmail.com>
2022-12-16 10:31:22 +0100

    Feat/Css-Style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,27 @@
-import React from "react"
+import React from "react";
 
 export default function Card(props) {
-
-    return (
-        <div className="card--item">
-
-             <div>
-               <h4 className="card--location">
-                  {props.location}
-               </h4>
-            </div>
-            <div>
-             <h2 className="card--title">
-                {props.title}
-             </h2>
-          </div>
-          <div className="card--date">
-              <p>`{props.startDate} - {props.endDate}`
-              </p>
-          </div>
-          <div className="card--description">
-            <p>
-             {props.description}
-            </p>
-          </div>
-          <div className="card--image">
-            <img src={props.imageUrl} className="image--card"/>
-            </div>
-
+  return (
+    <div className="card--item">
+      <div className="card--image">
+        <img src={props.imageUrl} className="image--card" />
+      </div>
+      <div className="card--texts">
+        <div>
+          <h4 className="card--location">📍{props.location}</h4>
         </div>
-    )
+        <div>
+          <h2 className="card--title">{props.title}</h2>
+        </div>
+        <div className="card--date">
+          <p>
+            {props.startDate} - {props.endDate}
+          </p>
+        </div>
+        <div className="card--description">
+          <p>{props.description}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/data.js
+++ src/data.js
@@ -1,29 +1,34 @@
 export default [
   {
-      title: "Mount Fuji",
-      location: "Japan",
-      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-      startDate: "12 Jan, 2021",
-      endDate: "24 Jan, 2021",
-      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
   },
   {
-      title: "South Africa",
-      location: "Cape Town",
-      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-      startDate: "12 Jan, 2021",
-      endDate: "24 Jan, 2021",
-      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      imageUrl: "https://images.unsplash.com/photo-1580060839134-75a5edca2e99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80"
+    title: "South Africa",
+    location: "Cape Town",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Known as the “Mother City”, Cape Town is the oldest city in South Africa. Perched between the ocean and the mountain, with a national park as its heart, there is nowhere like Cape Town. Cape Town, the “Mother City”, is the oldest city in South Africa and has a cultural heritage spanning more than 300 years.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1580060839134-75a5edca2e99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80",
   },
   {
-      title: "Australia",
-      location: "Melbourne",
-      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-      startDate: "12 Jan, 2021",
-      endDate: "24 Jan, 2021",
-      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      imageUrl: "https://images.unsplash.com/photo-1545044846-351ba102b6d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
-  }
-]
+    title: "Australia",
+    location: "Melbourne",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1545044846-351ba102b6d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80",
+  },
+];
